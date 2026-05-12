119d4dc04ca3bc6c6d0d7fec45a4c50156a185e7
kalwa2407 <talukdaraman24@gmail.com>
2026-05-12 11:15:17 +0530

    fix(auth): remove strict COOP header to prevent google auth popup block

--- next.config.mjs
+++ next.config.mjs
@@ -23,7 +23,7 @@ const nextConfig = {
         source: "/(.*)",
         headers: [
           { key: "X-Frame-Options", value: "DENY" },
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin-allow-popups" },
+          // { key: "Cross-Origin-Opener-Policy", value: "same-origin-allow-popups" },
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
           {
